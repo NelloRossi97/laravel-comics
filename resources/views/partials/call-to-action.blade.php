a742b34b8b65831560c80aa31174fe52143ce54f
NelloRossi <rossianiello97@gmail.com>
2023-05-22 18:43:14 +0200

    fixed call to action

--- resources/views/partials/call-to-action.blade.php
+++ resources/views/partials/call-to-action.blade.php
@@ -1,8 +1,8 @@
  <section class="py-3">
      <div class="container px-5 d-flex flex-column flex-xl-row justify-content-between align-items-center">
          @foreach ($contentCall as $content)
-             <div class="d-flex align-items-center prova">
-                 <img class="me-4 mb-4 h-50" src="/img/{{ $content['image'] }}" alt="">
+             <div class="d-flex align-items-center call justify-content-center">
+                 <img class="align-self-center me-3 h-50" src="/img/{{ $content['image'] }}" alt="">
                  <span class="text-uppercase">{{ $content['text'] }}</span>
              </div>
          @endforeach
@@ -10,7 +10,7 @@
  </section>
 
  <style lang="scss" scoped>
-     .prova:last-child img {
+     .call:last-child img {
          width: 70px;
          height: 70px;
      }
