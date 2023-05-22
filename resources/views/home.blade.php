d42c5a81a8a798cded789cfe07df2dd63556a12a
NelloRossi <rossianiello97@gmail.com>
2023-05-22 15:25:58 +0200

    fixed colors in headers and home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>main</h1>
+    <div class="container py-5 fw-bold fs-3 position-relative d-flex flex-column">
+        <span class="mylabel mybg-blue position-absolute px-4 py-1 text-uppercase">Current series</span>
+        <div class="row mt-5">
+
+        </div>
+        <button class="text-uppercase px-5 py-1 mybg-blue align-self-center fw-bold">Load More</button>
+    </div>
 @endsection
+<style lang="scss" scoped>
+    .mylabel {
+        top: -20px;
+        font-size: 1.5rem;
+        background-color: #0069cc;
+    }
+
+    .mybg-blue {}
+
+    button {
+        background-color: #0069cc;
+        font-size: 1.5rem;
+        border: none;
+        color: white;
+
+        &:hover {
+            background-color: #40a3ff;
+        }
+    }
+</style>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,8 +2,10 @@
     <nav class="d-flex justify-content-between align-items-center">
         <img src="/img/dc-logo.png" alt="DC Logo">
         <ul class="d-flex align-items-center list-unstyled m-0 h-100 d-none d-lg-flex">
-            <li class="me-3 h-100 d-flex align-items-center">
+            <li class="me-3 h-100 d-flex align-items-center text-center">
                 <a href="/" class="nav-link fw-bold text-uppercase me-3">Home</a>
+            </li>
+            <li class="h-100 d-flex align-items-center">
                 <a href="/comics-list" class="nav-link fw-bold text-uppercase">Comics List</a>
             </li>
         </ul>
@@ -13,41 +15,19 @@
 
 
 <style lang="scss" scoped>
-    @use '../assets/styles/colors.scss'as *;
-
     .border-transparent {
         border-bottom: 3px solid transparent;
     }
 
-    .active {
-        border-bottom: 3px solid $blue-secondary;
-        color: $blue-secondary;
+
+    ul {
+        color: #303030;
+
+
     }
 
-    .active-dropdown {
-        background-color: $blue-secondary;
-        color: white;
-    }
-
-    nav {
-        ul {
-            color: $bg-contacts;
-
-            li:hover {
-                border-bottom: 3px solid $blue-secondary;
-                color: $blue-secondary;
-            }
-        }
-    }
-
-    .dropdown {
-        ul {
-            right: 0px;
-        }
-
-        li:hover {
-            border: none;
-            color: white;
-        }
+    li:hover {
+        border-bottom: 3px solid #0069cc;
+        color: #0069cc;
     }
 </style>
